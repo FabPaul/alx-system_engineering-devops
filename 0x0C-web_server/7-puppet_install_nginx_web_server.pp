e12a5a83f13924c7855ef7426010f7c659df8e28
FabPaul <paulfopa0@gmail.com>
2023-08-29 21:55:15 +0100

    task5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,27 +1,6 @@
 # Puppet file that installs nginx web server
 
-package {'nginx':
-  ensure => 'present',
-}
-
-exec {'nginx install':
-  command  => 'sudo apt-get update; sudo apt-get -y install nginx',
+exec { 'server setup':
   provider => shell,
-}
-
-exec {'Intro':
-  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
-  provider => shell,
-}
-
-exec {'port 80 and redirect':
-  command  => 'sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https:\/\/twitter.com\/FabPaul1;\\n\\t}\\n" /etc/nginx/sites-available/default'
-  provider => shell,
-  require  => Exec['nginx install'], # to be sure ginx has first been installed
-}
-
-exec {'Nginx restart':
-  command  => 'sudo service nginx restart'
-  provider => shell,
-  require  => Exec['port 80 and redirect'], # to be sire redirection is configured and working
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.html; sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://twitter.com/FabPaul1;\\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart',
 }
