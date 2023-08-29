d9595b84d866f6c7ad905575bc792faa180b1b48
FabPaul <paulfopa0@gmail.com>
2023-08-29 22:08:06 +0100

    task5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,6 @@
 # Puppet file that installs nginx web server
 
 exec { 'server setup':
-  provider => shell,
   command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.html; sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://twitter.com/FabPaul1;\\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart',
+  provider => shell,
 }
