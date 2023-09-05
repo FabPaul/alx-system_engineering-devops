de184f7afb500cbf2566c6700958cb7fa00b2846
FabPaul <paulfopa0@gmail.com>
2023-09-05 10:31:50 +0100

    task2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -13,8 +13,8 @@ package { 'nginx':
 # Create a file_line resource to add the custom httpheader to nginx
 file_line { 'http_header':
   path  => '/etc/nginx/nginx.conf',
-  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";"
-  match => 'http {',
+  line  => "add_header X-Served-By \"${hostname}\";",
+  match => '^.*http\s*{.*$',
 }
 
 # Restart nginx
