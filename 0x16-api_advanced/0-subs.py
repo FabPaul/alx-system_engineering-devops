b982196ee1091feb09ae533bb26e42980074b319
FabPaul <paulfopa0@gmail.com>
2023-10-10 11:25:31 +0100

    task1

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit. If an invalid
+subreddit is given, the function should return 0. """
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """function to return the number of subscribers for a given subreddit """
+    base_url = "https://www.reddit.com/"
+    user_url = f"{base_url}/r/{subreddit}/about.json"
+
+    user_agent = {"user-agent": "Python/requests"}
+
+    response = requests.get(user_url, headers=user_agent,
+                           allow_redirects=False)
+
+    if not subreddit or not isinstance(subreddit, str):
+        return 0
+
+    return response.json().get("data").get("subscribers")
