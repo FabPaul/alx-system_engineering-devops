46b0004ec7072069555503671b2f675d06869e21
FabPaul <paulfopa0@gmail.com>
2023-10-11 10:14:33 +0100

    task 3 errors

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,39 +5,45 @@ import json
 import requests
 
 
-def count_words(subreddit, word_list, params={}):
+def count_words(subreddit, word_list, params=None):
     """
     function that queries the Reddit API, parses the title of all hot articles,
     and prints a sorted count of given keywords (case-insensitive, delimited
     by spaces. Javascript should count as javascript, but java should not).
     """
-    word_count = {word: 0 for word in word_list}
-    total_words = len(word_count)
-    limits = {}
+    if params is None:
+        params = {}
 
     base_url = "https://www.reddit.com/"
-    user_url = f"{base_url}/r/{subreddit}.hot.json"
+    user_url = f"{base_url}r/{subreddit}/hot.json"
 
     user_agent = {"User-Agent": "Python/requests"}
 
     response = requests.get(user_url, headers=user_agent, params=params,
                             allow_redirects=False)
-    title = []
 
     if response.status_code == 200:
-        response = response.json()
-        hot_articles = response.get("data").get("children")
+        response_data = response.json()
+        children = response_data.get("data", {}).get("children", [])
 
-        for article in range(len(children)):
-            titles = children[article].get("data").get("title").split()
-            print(f"title:{titles}")
-            after = response.get("data").get("after")
+        word_count = {word: 0 for word in word_list}
 
-        if not after:
-            return None
+        for article in children:
+            title = article.get("data", {}).get("title", "")
+            title_words = [word.strip('.!_') for word in title.lower().split()]
+            for word in word_list:
+                if word.lower() in title_words:
+                    word_count[word] += title_words.count(word.lower())
+
+        after = response_data.get("data", {}).get("after")
+        if after:
+            params["after"] = after
+            count_words(subreddit, word_list, params)
         else:
-            limit = {"after": after, "a": a}
-            count_words(subreddit, word_list, limit)
-            return None
+            # Sort and print the results
+            sorted_counts = sorted(word_count.items(),
+                                   key=lambda x: (-x[1], x[0]))
+            for word, count in sorted_counts:
+                print(f"{word.lower()}: {count}")
     else:
-        return None
+        return
