a60c665c474f598184afef4a47f459ff4b9c3113
FabPaul <paulfopa0@gmail.com>
2023-10-19 17:41:08 +0100

    task0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,15 +1,15 @@
 # fix a Nginx server that was getting many failed requests
 exec {'increase ulimit':
   path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-  command => 'sudo sed -i "s/-n 15/-n 30000/g" /etc/default/nginx'
+  command => 'sed -i "s/-n 15/-n 30000/g" /etc/default/nginx'
 }
 
 exec {'add equal sign to assign value':
   path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-  command => 'sudo sed -i "s/ulimit $ULIMIT/ulimit=$ULIMIT/g" /etc/init.d/nginx'
+  command => 'sed -i "s/ulimit $ULIMIT/ulimit=$ULIMIT/g" /etc/init.d/nginx'
 }
 
 exec {'reload and restart Nginx':
   path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-  command => 'sudo service nginx reload; sudo service nginx restart'
+  command => 'service nginx reload; service nginx restart'
 }
